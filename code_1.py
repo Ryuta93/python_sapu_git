a9049f1f4171f2072a517f0d343c6ff7f76f6f92
Ryuta93 <sentianlongtai93@gmail.com>
2024-11-14 06:07:01 +0900

    問題1-2

--- code_1.py
+++ code_1.py
@@ -1,7 +1,22 @@
 # 問題1-1
-
+"""
 scores = {"数学":82, "国語":74, "英語":60, "理科":92, "社会":70}
 
 diff = scores["理科"] - scores["社会"]
 
-print(diff)
+print(f"{diff}点")
+"""
+# 問題1-2
+"""
+scores = [82, 74, 60, 92, 70]
+avg_score = sum(scores) / len(scores)
+print(avg_score)    
+"""
+scores = {"数学":82, "国語":74, "英語":60, "理科":92, "社会":70}
+"""
+scores_values = list(scores.values())
+avg_score = sum(scores_values) / len(scores_values)
+print(f'{avg_score}点')
+"""
+avg_score = sum(scores.values()) / len(scores.values())
+print(f'{avg_score}点')
