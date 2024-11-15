62836556a511be3020b262c618e3027c861c5e07
Ryuta93 <sentianlongtai93@gmail.com>
2024-11-15 14:28:28 +0900

    問題2-1

--- code_1.py
+++ code_1.py
@@ -1,12 +1,11 @@
-names = ['斎藤', '佐藤', '鈴木']
+#問題2-1
+year = 1000
 
-for x in names:
-    print(f'{x}さん')
-    
-scores = {'Math': 82, 'Japanese': 74, 'English': 60, 'Science': 92, 'Society': 70}
-
-for k, v in scores.items():
-    print(f'{k}の点数は{v}点です')
-    
-for i in range(1, 11):
-    print(i)
+if year % 400 == 0:
+    print('閏年です')
+elif year % 100 == 0:
+    print('平年です')
+elif year % 4 == 0:
+    print('閏年です')
+else:
+    print('平年です')
