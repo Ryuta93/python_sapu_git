46f1b63ba74f36968b3c2e1861244cf2bbb53c26
Ryuta93 <sentianlongtai93@gmail.com>
2024-11-15 15:00:26 +0900

    関数の練習

--- code_1.py
+++ code_1.py
@@ -1,22 +1,11 @@
-# 問題2-1
-"""year = 1000
-
-if year % 400 == 0:
-    print('閏年です')
-elif year % 100 == 0:
-    print('平年です')
-elif year % 4 == 0:
-    print('閏年です')
-else:
-    print('平年です')"""
-    
-# 問題2-2 FizzBuzz
-for number in range(1, 101):
-    if number % 15 == 0:
-        print('FizzBuzz')
-    elif number % 3 == 0:
-        print('Fizz')
-    elif number % 5 == 0:
-        print('Buzz')
+def is_leap_years(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
     else:
-        print(number)
+        return False
+    
+print(is_leap_years(2024))
